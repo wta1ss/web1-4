a0da33a208ebd0715e19ea67fc431ff8a4c6b5fe
Unknown <bermuda_rars@outlook.jp>
2018-05-21 17:00:31 +0900

    comit

---
+++ Roulette/Assets/RouletteController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour {
+
+	float rotSpeed = 0; // 回転速度
+
+	void Start() {
+	}
+
+	void Update() {
+		// マウスが押されたら回転速度を設定する。
+		if(Input.GetMouseButtonDown(0)) {
+			this.rotSpeed = 10;
+		}
+
+		// 回転速度分、ルーレットを回転させる
+		transform.Rotate(0, 0, this.rotSpeed);
+
+		// ルーレットを減速させる（追加）
+		this.rotSpeed *= 0.96f;
+	}
+}
